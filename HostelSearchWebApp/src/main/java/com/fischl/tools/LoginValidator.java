c129d88a3e8a56ee15ac2cdd3fdb62b0837536a1
MinhNKfptu <minhnkce170269@fpt.edu.vn>
2023-10-16 10:51:30 +0700

    fix LoginValidator >> if not found account, returns -1

--- HostelSearchWebApp/src/main/java/com/fischl/tools/LoginValidator.java
+++ HostelSearchWebApp/src/main/java/com/fischl/tools/LoginValidator.java
@@ -1,10 +1,15 @@
 package com.fischl.tools;
 
 import com.fischl.DAOs.AccountDAO;
+import com.fischl.models.Account;
+
 public class LoginValidator {
 
     public int checkAccount(String user_name, String password) {
+        Account acc = null;
 
-        return new AccountDAO().getByUsernamePassword(user_name,password).getUserId();
+        acc = new AccountDAO().getByUsernamePassword(user_name,password);
+
+        return (acc == null) ? -1 : acc.getUserId();
     }
 }
