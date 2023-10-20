42b2481672a6c69e8da5e680500c32da324f60d4
unknown <thanhnttce170901@fpt.edu.vn>
2023-10-20 16:51:25 +0700

    add js function add JavaScript function to assets/js/admin/js (addUser,updateUser,deleteUser,addHostel,updateHostel,deleteHostel,searchHostel,setDefaultUserType,selectRole)

--- HostelSearchWebApp/src/main/webapp/assets/js/admin.js
+++ HostelSearchWebApp/src/main/webapp/assets/js/admin.js
@@ -4,10 +4,10 @@ function searchPosts() {
     var rows = tableBody.getElementsByTagName("tr");
 
     for (var i = 0; i < rows.length; i++) {
+        var id = removeDiacritics(rows[i].getElementsByTagName("td")[0].innerText.toLowerCase());
         var title = removeDiacritics(rows[i].getElementsByTagName("td")[1].innerText.toLowerCase());
-        var address = removeDiacritics(rows[i].getElementsByTagName("td")[3].innerText.toLowerCase());
 
-        if (title.includes(input) || address.includes(input)) {
+        if (title.includes(input) || id.includes(input)) {
             rows[i].style.display = "";
         } else {
             rows[i].style.display = "none";
@@ -15,16 +15,17 @@ function searchPosts() {
     }
 }
 
-function searchUsers() {
+function searchHostel() {
     var input = removeDiacritics(document.getElementById("userSearchQuery").value.toLowerCase());
     var tableBody = document.getElementById("usersTableBody");
     var rows = tableBody.getElementsByTagName("tr");
 
     for (var i = 0; i < rows.length; i++) {
-        var username = removeDiacritics(rows[i].getElementsByTagName("td")[1].innerText.toLowerCase());
-        var fullName = removeDiacritics(rows[i].getElementsByTagName("td")[4].innerText.toLowerCase());
+        var id = removeDiacritics(rows[i].getElementsByTagName("td")[0].innerText.toLowerCase()); // Lấy giá trị ID từ cột đầu tiên của bảng
+        var hostelName = removeDiacritics(rows[i].getElementsByTagName("td")[1].innerText.toLowerCase());
 
-        if (username.includes(input) || fullName.includes(input)) {
+
+        if (hostelName.includes(input) || id.includes(input)) {
             rows[i].style.display = "";
         } else {
             rows[i].style.display = "none";
@@ -47,15 +48,73 @@ function deletePost(postId) {
     // For example, you can redirect to an update page with the post ID
     window.location.href = "deletePost?id=" + postId;
 }
+//User ADMIN PAGE
+function addUser() {
+    // Perform the necessary logic to update the post
+    // For example, you can redirect to an update page with the post ID
+    window.location.href = "addUser";
+}
 
 function updateUser(userId) {
     // Perform the necessary logic to update the post
     // For example, you can redirect to an update page with the post ID
-    window.location.href = "changeInfo?id=" + userId;
+    window.location.href = "updateUser?id=" + userId;
 }
 
 function deleteUser(userId) {
     // Perform the necessary logic to update the post
     // For example, you can redirect to an update page with the post ID
-    window.location.href = "deleteUser?id=" + userId;
+    if(confirm("Bạn có chắc sẽ xóa người dùng này không? ") + userId){
+        window.location.href = "deleteUser?id=" + userId;
+    }
+    
 }
+//User ADMIN PAGE END
+/////////////////////////////////////////
+//HOSTEL ADMIN PAGE
+function addHostel() {
+    // Perform the necessary logic to update the post
+    // For example, you can redirect to an update page with the post ID
+    window.location.href = "addUser";
+}
+
+function updateHostel(hostelId) {
+    // Perform the necessary logic to update the post
+    // For example, you can redirect to an update page with the post ID
+    window.location.href = "updateUser?id=" + hostelId;
+}
+
+function deleteHostel(hostelId) {
+    // Perform the necessary logic to update the post
+    // For example, you can redirect to an update page with the post ID
+    if(confirm("Bạn có chắc sẽ xóa người dùng này không? ") + hostelId){
+        window.location.href = "deleteUser?id=" + hostelId;
+    }
+    
+}
+///HOSTEL ADMIN PAGE END
+function setDefaultUserType() {
+    var userTypeSelect = document.getElementById("userType");
+    var selectedValue = userTypeSelect.value;
+    
+    if (selectedValue === "") {
+        // Nếu không có giá trị được chọn, thiết lập giá trị mặc định là "admin"
+        userTypeSelect.value = "admin";
+    }
+}
+function selectRole(role) {
+    document.getElementById("userType").value = role;
+    // Optional: You can add visual cues to indicate the selected role, for example, changing button styles.
+    if (role === "admin") {
+        document.getElementById("adminButton").classList.add("selected");
+        document.getElementById("normalButton").classList.remove("selected");
+    } else {
+        document.getElementById("adminButton").classList.remove("selected");
+        document.getElementById("normalButton").classList.add("selected");
+    }
+}
+
+
+
+
+
